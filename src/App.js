0d5dbfd53fb48da3658914efbe1abbdefc28c97b
ibrahimyc <ibrahimyucetepe@gmail.com>
2025-06-27 16:06:12 +0300

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,1 +1,13 @@
+import React from 'react';
+import KPIDashboard from './components/KPIDashboard';
+import './App.css';
 
+function App() {
+  return (
+    <div className="App">
+      <KPIDashboard />
+    </div>
+  );
+}
+
+export default App;
